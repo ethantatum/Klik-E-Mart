d96064780fd33ca84d4990ff4cae03c39d8919f8
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-03-28 11:58:42 -0700

    updated App with logic, created Container component

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from './components/Image';
+import Container from './components/Container';
 import images from './images.json';
 
 
@@ -11,10 +13,56 @@ class App extends React.Component {
         currentScore: 0
     };
 
+    clickImage = id => {
+        const shuffleArr = this.shuffleImages(images);
+        this.setState({ images: shuffleArr });
+        if(this.state.clickedArr.includes(id)) {
+            this.setState({ currentScore: 0, clickedArr: [] });
+        } else {
+            this.setState({
+                clickedArr: this.state.clickedArr.concat([id]),
+                currentScore: this.state.currentScore + 1
+            });
+        }
+        if(this.state.currentScore > this.state.highScore) {
+            this.setState({ highScore: this.state.currentScore });
+        }
+    }
+
+    shuffleImages = (imageArr) => {
+        for(let i = imageArr.length -1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [imageArr[i], imageArr[j]] = [imageArr[j], imageArr[i]];
+        }
+        return imageArr;
+    }
+
     render() {
         return (
-            <img src={this.state.pictures[0]} className='img-fluid' alt='apu1' />
-        )
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Who Needs the Klik-E-Mart? You dooooooo!</h1>
+                </header>
+                <h3 className="App-intro">
+                    <strong>Click on an image to earn points, but don't click on any image more than once!</strong>
+                    <p className="Current-score">
+                        <strong>Score: {this.state.currentScore} | High Score: {this.state.highScore}</strong>
+                    </p>
+                </h3>
+                <Container  
+                    imageGrid={this.state.images.map(image => (
+                        <Image
+                            clickImage={this.clickImage}
+                            id={image.id}
+                            key={image.id}
+                            name={image.name}
+                            image={image.image}
+                        />
+                    ))}
+                />
+                
+            </div>
+        );
     }
 }
 
---
+++ clicky/src/components/Container/Container.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Container = props => <div className="container-fluid bg-light mt-5 p-2"></div>;
+
+export default Container;

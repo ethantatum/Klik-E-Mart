71c361f79ffaf052a5c0f2a613741466af4822ba
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-03-29 11:33:54 -0700

    Updates

---
+++ precache-manifest.2297e810c8634a0ac7083eb57aee02fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f831e5d0b1ce9aa3b15",
+    "url": "/Klik-E-Mart/static/js/runtime~main.55db3dd2.js"
+  },
+  {
+    "revision": "4f97082cd1531bd5f7a9",
+    "url": "/Klik-E-Mart/static/js/main.6024e83a.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Klik-E-Mart/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "4f97082cd1531bd5f7a9",
+    "url": "/Klik-E-Mart/static/css/main.ebb04d62.chunk.css"
+  },
+  {
+    "revision": "f465ead08044cceb1e25e667f2b1e516",
+    "url": "/Klik-E-Mart/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Klik-E-Mart/precache-manifest.2297e810c8634a0ac7083eb57aee02fc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Klik-E-Mart/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

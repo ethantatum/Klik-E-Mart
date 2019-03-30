0ea49d76c6294f252a28fbfc2f78daef93e6868a
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-03-30 09:46:53 -0700

    updated game logic for winning

--- src/App.js
+++ src/App.js
@@ -15,39 +15,43 @@ class App extends React.Component {
         message: ""
     };
 
-    componentDidMount () {
+    componentDidMount() {
         console.log(this.state.images)
     }
 
     clickImage = id => {
         const shuffleArr = this.shuffleImages(images);
         this.setState({ images: shuffleArr });
-        if(this.state.clickedArr.includes(id)) {
-            this.setState({ 
-                currentScore: 0, 
-                clickedArr: [], 
-                message: "D'oh! You already clicked that one...try again!" 
-            });
-        } else if(this.state.currentScore === 12) {
+        if (this.state.clickedArr.includes(id)) {
             this.setState({
-                message: "Excellent...you won the game!",
                 currentScore: 0,
-                clickedArr: []
+                clickedArr: [],
+                message: "D'oh! You already clicked that one...try again!"
             });
+
         } else {
-            this.setState({
-                clickedArr: this.state.clickedArr.concat([id]),
-                currentScore: this.state.currentScore + 1,
-                message: "Woohoo!"
-            });
-        }
-        if(this.state.currentScore > this.state.highScore) {
-            this.setState({ highScore: this.state.currentScore + 1 });
+            // this happens normally
+            if (this.state.currentScore < 11) {
+                this.setState({
+                    clickedArr: this.state.clickedArr.concat([id]),
+                    currentScore: this.state.currentScore + 1,
+                    message: "Woohoo!"
+                })
+            } else {
+                this.setState({
+                    message: "Excellent...you won the game!",
+                    currentScore: 0,
+                    clickedArr: []
+                });
+
+            }
+            if (this.state.currentScore >= this.state.highScore) {
+                this.setState({ highScore: this.state.currentScore + 1 });
+            }
         }
     }
-
     shuffleImages = (imageArr) => {
-        for(let i = imageArr.length -1; i > 0; i--) {
+        for (let i = imageArr.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
             [imageArr[i], imageArr[j]] = [imageArr[j], imageArr[i]];
         }
@@ -58,7 +62,7 @@ class App extends React.Component {
         return (
             <div className="App">
                 <header className="container mt-2">
-                    <img className="title-image img-fluid" src='./images/sign2.png' alt="klik-e-mart"/>
+                    <img className="title-image img-fluid" src='./images/sign2.png' alt="klik-e-mart" />
                 </header>
                 <h3 className="container text-center mt-4">
                     <strong>Click on an image to earn points, but don't click on any image more than once!</strong>
@@ -78,13 +82,14 @@ class App extends React.Component {
                             name={image.name}
                             image={image.image}
                         />
-                        )
+                    )
                     )}
-                </Container>  
-                
+                </Container>
+
             </div>
         );
-    }
+    };
 }
+
 
 export default App;
